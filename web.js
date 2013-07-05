bca3c5466af2570acf9619d5cb51ee928124eadb
janoe <jose.antonio.noe@gmail.com>
2013-07-06 00:29:11 +0200

    Homework 3 parte 1, leemos del fichero index.html y lo metemos en un buffer

--- web.js
+++ web.js
@@ -1,10 +1,16 @@
+/*jshint node:true*/
+
+var fs = require('fs');
 var express = require('express');
+
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Pasa puessssss!');
+app.get('/', function(request, response){
+    var buffer = fs.readFileSync("index.html");
+    response.write(buffer);
 });
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
